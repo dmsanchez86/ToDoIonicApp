9086525226af3dddf7a2c7066041424ecb351f44
Daniel M Sanchez <dsanchez@existaya.com>
2018-09-19 14:09:54 -0500

    se creo servicio para el To Do

---
+++ src/services/todo.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core'; 
+import { List } from '../models/';
+
+@Injectable()
+
+export class ToDoService {
+    lists: List[] = [];
+
+    constructor() {
+        const list1 = new List('Recolectar piedras del infinito');
+        const list2 = new List('Heroes a Vencer');
+
+        this.lists.push(list1, list2);
+    }
+}
