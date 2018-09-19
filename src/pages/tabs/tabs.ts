bdb086ec3afec948d4365a52c172cd08a3193319
Daniel M Sanchez <dsanchez@existaya.com>
2018-09-19 13:46:39 -0500

    se organizo el template y componente de la navegacion

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { PendientsPage } from '../pendients/pendients';
+import { FinishersPage } from '../finishers/finishers';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = PendientsPage;
+  tab2Root = FinishersPage;
 
   constructor() {
 
