dd3109f821911bf0ff0f60563c58e22974652ab6
Daniel M Sanchez <dsanchez@existaya.com>
2018-09-19 14:01:22 -0500

    se agrego servicio a la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { FinishersPage } from '../pages/finishers/finishers';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+// servicios
+import { ToDoService } from '../services/todo.service';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -31,6 +34,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    ToDoService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
