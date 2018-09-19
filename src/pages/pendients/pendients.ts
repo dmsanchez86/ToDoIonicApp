1ca3f2445574e981a010da73b7317501bcaf445b
Daniel M Sanchez <dsanchez@existaya.com>
2018-09-19 14:10:09 -0500

    se inyecto servicio en la pagina de pendientes

--- src/pages/pendients/pendients.ts
+++ src/pages/pendients/pendients.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
+import { ToDoService } from '../../services/todo.service';
 
 @Component({
     selector: 'page-pendients',
     templateUrl: 'pendients.html'
 })
 export class PendientsPage{
-    
+    constructor(private _toDoService: ToDoService) {}
 }
